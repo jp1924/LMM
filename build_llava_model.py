c3f83970bc127a6708504b9f46416d3c914170de
jp1924 <jsb10121249@gmail.com>
2024-09-11 15:41:57 +0000

    Clean: code

--- build_llava_model.py
+++ build_llava_model.py
@@ -20,7 +20,7 @@ from transformers import (
 
 
 IMG_TOKEN = "<|image|>"
-CHAT_TEMPLATE = """{% for message in messages %}{% if message.role == 'user' %}{{ '### User:\n' }}{% if message.content is not string %}{% for content in message.content %}{% if content.type == 'image' %}{{ img_token }}{% elif content.type == 'text' %}{{ content.text }}{% else %}{# Do nothing #}{% endif %}{% endfor %}{% else %}{{ message.content }}{% endif %}{{ '\n\n' }}{% elif message.role == 'assistant' %}{{ '### Assistant:\n' }}{% if message.content is not string %}{% for content in message.content %}{% if content.type == 'image' %}{{ img_token }}{% elif content.type == 'text' %}{{ content.text }}{% else %}{# Do nothing #}{% endif %}{% endfor %}{% else %}{{ message.content }}{% endif %}{% endif %}{% endfor %}{{ eos_token }}"""
+CHAT_TEMPLATE = """{% if not add_last_empty_assistant is defined %}{% set add_last_empty_assistant = false %}{% endif %}{% for message in messages %}{{ sot_token }}{% if message.role == 'user' %}{{ '### User:\n' }}{% if message.content is not string %}{% for content in message.content %}{% if content.type == 'image' %}{{ img_token }}{% elif content.type == 'text' %}{{ content.text }}{% else %}{# Do nothing #}{% endif %}{% endfor %}{% else %}{{ message.content }}{% endif %}{{ '\n\n' }}{% elif message.role == 'system' %}{{ '### System:\n' }}{% if message.content is not string %}{% for content in message.content %}{% if content.type == 'image' %}{{ img_token }}{% elif content.type == 'text' %}{{ content.text }}{% else %}{# Do nothing #}{% endif %}{% endfor %}{% else %}{{ message.content }}{% endif %}{{ '\n\n' }}{% elif message.role == 'assistant' %}{{ '### Assistant:\n' }}{% if message.content is not string %}{% for content in message.content %}{% if content.type == 'text' %}{{ content.text }}{% else %}{# Do nothing #}{% endif %}{% endfor %}{% if not loop.last %}{{ '\n\n' }}{% endif %}{% else %}{{ message.content }}{% if not loop.last %}{{ '\n\n' }}{% endif %}{% endif %}{% else %}{# Do nothing #}{% endif %}{{ eot_token }}{% endfor %}{% if not add_last_empty_assistant %}{{ eos_token }}{% elif add_last_empty_assistant %}{{ '### Assistant:\n' }}{% else %}{# Do nothing #}{% endif %}"""
 
 
 def get_vision_processor_and_model(vision_model_name_or_path) -> Tuple[AutoModel, AutoImageProcessor, AutoConfig]:
@@ -71,40 +71,34 @@ def get_language_tokenizer_and_model(language_model_name_or_path) -> Tuple[AutoM
     return (language_model, language_config, tokenizer)
 
 
-def main() -> None:
-    vision_model_name_or_path = "Bingsu/clip-vit-large-patch14-ko"
-    language_model_name_or_path = "beomi/llama-2-koen-13b"
-
+def main(
+    language_model_name_or_path: str,
+    vision_model_name_or_path: str,
+    output_dir: str,
+):
     vision_tower, vision_config, image_processor = get_vision_processor_and_model(vision_model_name_or_path)
     language_model, language_config, tokenizer = get_language_tokenizer_and_model(language_model_name_or_path)
 
-    # from mplugDocOwl
+    # NOTE: LLaVA-NEXT Abalation 논문을 보고 수정함.
     image_grid_pinpoints = (
         torch.tensor(
             [
                 (1, 1),
                 (1, 2),
                 (2, 1),
+                (2, 2),
                 (1, 3),
                 (3, 1),
-                (2, 2),
-                (1, 4),
-                (4, 1),
-                (1, 5),
-                (5, 1),
-                (1, 6),
-                (6, 1),
                 (2, 3),
                 (3, 2),
-                (1, 7),
-                (7, 1),
-                (4, 2),
-                (2, 4),
-                (1, 8),
-                (8, 1),
                 (3, 3),
-                (1, 9),
-                (9, 1),
+                (1, 4),
+                (4, 1),
+                (2, 4),
+                (4, 2),
+                (3, 4),
+                (4, 3),
+                (4, 4),
             ]
         )
         * vision_config.image_size
@@ -114,66 +108,55 @@ def main() -> None:
     setattr(image_processor, "image_grid_pinpoints", image_grid_pinpoints)
     setattr(image_processor, "do_pad", True)
 
-    # stage-1 학습을 위해, vision model의 image_prcessor로 저장함, 반대로 llava next image processor로 불러오는 것도 가능함.
-    # llava_image_processor = LlavaNextImageProcessor(
-    #     do_resize=image_processor.do_resize,
-    #     size=image_processor.size,
-    #     resample=image_processor.resample,
-    #     do_center_crop=image_processor.do_center_crop,
-    #     do_rescale=image_processor.do_rescale,
-    #     rescale_factor=image_processor.rescale_factor,
-    #     do_normalize=image_processor.do_normalize,
-    #     image_mean=image_processor.image_mean,
-    #     image_std=image_processor.image_std,
-    #     do_convert_rgb=image_processor.do_convert_rgb,
-    #     image_grid_pinpoints=image_grid_pinpoints,
-    #     do_pad=True,
-    # )
-    processor = LlavaProcessor(
-        tokenizer=tokenizer,
-        image_processor=image_processor,
-        chat_template=tokenizer.chat_template,
-    )
-
     config = LlavaConfig(
         vision_config=vision_config,
+        image_seq_length=vision_config.image_size,
         text_config=language_config,
         image_grid_pinpoints=image_grid_pinpoints,
         image_token_index=tokenizer.convert_tokens_to_ids(IMG_TOKEN),
     )
+    processor = LlavaProcessor(
+        tokenizer=tokenizer,
+        image_processor=image_processor,
+        image_token=IMG_TOKEN,
+        vision_feature_select_strategy=config.vision_feature_select_strategy,
+        patch_size=config.vision_config.patch_size,
+        chat_template=tokenizer.chat_template,
+    )
+
     model = LlavaForConditionalGeneration(config)
 
     # TODO: 나중에 set_decoder로 바꿀 것.
     model.vision_tower = vision_tower
     model.language_model = language_model
 
-    hub_name = "Llama-ClipLarge-Llava"
-    output_dir = f"/root/output_dir/{hub_name}"
     model.save_pretrained(output_dir)
     processor.save_pretrained(output_dir)
+
+    return (model, processor)
+
+
+if "__main__" in __name__:
+    vision_model_name_or_path = "Bingsu/clip-vit-large-patch14-ko"
+    language_model_name_or_path = "beomi/llama-2-ko-7b"
+
+    hub_name = "KoLLaVA-7b"
+    output_dir = f"/root/output_dir/llava/{hub_name}"
+    try:
+        processor = LlavaProcessor.from_pretrained(output_dir)
+        model = LlavaForConditionalGeneration.from_pretrained(output_dir)
+    except BaseException as e:  # noqa: F841
+        model, processor = main(
+            language_model_name_or_path,
+            vision_model_name_or_path,
+            output_dir,
+        )
 
     max_retry = 10
     for retries in range(max_retry):
         try:
             model.push_to_hub(hub_name, private=True)
             processor.push_to_hub(hub_name, private=True)
-        except BaseException as e:
-            print(f"해당 애러가 {retries}시에 발생: {e}")
-    else:
-        exit("모델이 정상적으로 업로드 되질 않았음. 프로그램을 종료함.")
-
-
-if "__main__" in __name__:
-    try:
-        processor = LlavaProcessor.from_pretrained("/root/output_dir/Llama-ClipLarge-Llava")
-        model = LlavaForConditionalGeneration.from_pretrained("/root/output_dir/Llama-ClipLarge-Llava")
-    except BaseException as e:
-        main()
-    max_retry = 10
-    for retries in range(max_retry):
-        try:
-            model.push_to_hub("Llama-ClipLarge-Llava", private=True)
-            processor.push_to_hub("Llama-ClipLarge-Llava", private=True)
         except BaseException as e:
             print(f"해당 애러가 {retries}시에 발생: {e}")
     else:
