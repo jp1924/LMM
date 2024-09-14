1d0a4fd3dc5b52c5a6d15dfdb1234ec3a505116b
jp1924 <jsb10121249@gmail.com>
2024-09-14 19:26:55 +0000

    Update: args

--- scripts/run_llava_stage_1.0_train.sh
+++ scripts/run_llava_stage_1.0_train.sh
@@ -5,7 +5,7 @@ export TORCHDYNAMO_DISABLE="1"
 
 export OMP_NUM_THREADS=2
 
-deepspeed --include=localhost:0,1,2,3 --master_port=10234 \
+deepspeed --num_gpus=8 \
     /root/workspace/llava_stage_1.0_train.py \
     --output_dir=/root/output_dir/llava/stage-1 \
     --run_name=llava-stage-1.0 \
@@ -14,16 +14,17 @@ deepspeed --include=localhost:0,1,2,3 --master_port=10234 \
     --cache_file_name=preprocessor.arrow \
     --preprocessing_batched=true \
     --preprocessing_num_workers=20 \
-    --preprocessing_batch_size=1000 \
-        jp1924/KoreanVisionDataforImageDescriptionSentenceExtractionandGeneration \
-        jp1924/OutsideKnowledgebasedMultimodalQAData \
-        jp1924/VisualQuestionAnswering \
-        jp1924/KoLLaVA-CC3M \
-        jp1924/Laion2BMultiKoreanSubset \
+    --preprocessing_batch_size=100 \
+    --dataset_repo_ls \
         jp1924/Coyo700m-1 \
+        jp1924/KoLLaVA-CC3M \
+        jp1924/VisualQuestionAnswering \
+        jp1924/Laion2BMultiKoreanSubset \
+        jp1924/OutsideKnowledgebasedMultimodalQAData \
+        jp1924/KoreanVisionDataforImageDescriptionSentenceExtractionandGeneration \
     --data_truncate_map='{"jp1924/KoreanVisionDataforImageDescriptionSentenceExtractionandGeneration": {"train": 2000000}, "jp1924/OutsideKnowledgebasedMultimodalQAData": {"train": 500000}, "jp1924/VisualQuestionAnswering": {"train": 500000}, "jp1924/KoLLaVA-CC3M": {"train": 500000}, "jp1924/Laion2BMultiKoreanSubset": {"train": 500000}, "jp1924/Coyo700m-1": {"train": 2000000}}' \
     --train_dataset_prefix=train \
-    --per_device_train_batch_size=16 \
+    --per_device_train_batch_size=24 \
     --gradient_accumulation_steps=4 \
     --per_device_eval_batch_size=2 \
     --num_train_epochs=1 \
@@ -44,9 +45,10 @@ deepspeed --include=localhost:0,1,2,3 --master_port=10234 \
     --logging_steps=1 \
     --bf16=true \
     --tf32=true \
-    --ddp_timeout=1800000 \
+    --ddp_timeout=18000000 \
     --use_liger_kernel=false \
     --gradient_checkpointing=true \
     --gradient_checkpointing_kwargs='{"use_reentrant": false}' \
     --group_by_length=false \
+    --torch_empty_cache_steps=50 \
     --deepspeed=/root/workspace/ds_config/ZeRO_2_act_check.json
